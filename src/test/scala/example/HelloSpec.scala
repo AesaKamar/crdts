b3eec15f6b716c280077485728b4e2dfb270eba3
AesaKamar <Aesa.Kamar@gmail.com>
2021-09-07 09:25:51 -0400

    todo:  semilattice.intercalateRepeat2

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -4,22 +4,30 @@ import cats.kernel.{Eq, Monoid, Semigroup, Semilattice}
 import cats.kernel.laws.discipline._
 import org.scalatest.funspec.{AnyFunSpec, AsyncFunSpec}
 import cats.laws
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.prop.Configuration
 import org.typelevel.discipline.scalatest.FunSpecDiscipline
+
+import java.util.UUID
 
 /** Must be:
   *   - A commutative Semigroup with an idempotent combine
   */
-final case class MySemiLattice(i: Int)
+final case class MySemiLattice(i: Int, seenIds: Set[UUID])
+
 object MySemiLattice {
   implicit val eqInstance: Eq[MySemiLattice] = new Eq[MySemiLattice] {
     override def eqv(x: MySemiLattice, y: MySemiLattice): Boolean = x.i == y.i
   }
 
   implicit val semigroupInstance: Semigroup[MySemiLattice] = new Semigroup[MySemiLattice] {
-    override def combine(x: MySemiLattice, y: MySemiLattice): MySemiLattice = MySemiLattice(
-      x.i + y.i
-    )
+    override def combine(x: MySemiLattice, y: MySemiLattice): MySemiLattice =
+      if (x.seenIds.isEmpty && y.seenIds.isEmpty && x.i != y.i)
+        MySemiLattice(x.i + y.i, Set.empty)
+      else if (x.seenIds.diff(y.seenIds).isEmpty && x.i == y.i)
+        x
+      else
+        MySemiLattice(x.i + y.i, y.seenIds.union(y.seenIds))
   }
 
   implicit val semilatticeInstance: Semilattice[MySemiLattice] = new Semilattice[MySemiLattice] {
@@ -28,7 +36,7 @@ object MySemiLattice {
   }
 
   implicit val monoidInstance: Monoid[MySemiLattice] = new Monoid[MySemiLattice] {
-    override def empty: MySemiLattice = MySemiLattice(0)
+    override def empty: MySemiLattice = MySemiLattice(0, Set.empty)
 
     override def combine(x: MySemiLattice, y: MySemiLattice): MySemiLattice =
       semigroupInstance.combine(x, y)
@@ -36,7 +44,16 @@ object MySemiLattice {
 }
 
 class HelloSpec extends AnyFunSpec with FunSpecDiscipline with Configuration {
-  import org.scalacheck.ScalacheckShapeless.derivedArbitrary
+//  import org.scalacheck.ScalacheckShapeless.derivedArbitrary
+
+  val mySemiLaticeGenInstance: Gen[MySemiLattice] = for {
+    i          <- Gen.long.map(_.toInt)
+    seenBefore <- Gen.listOf(Gen.uuid).map(_.toSet)
+  } yield MySemiLattice(i, seenBefore)
+
+  implicit val mySemiLaticeArbitraryInstance: Arbitrary[MySemiLattice] = Arbitrary(
+    mySemiLaticeGenInstance
+  )
 
   val catsLawsRuleSetSemigroup   =
     SemigroupTests[MySemiLattice](MySemiLattice.semigroupInstance).semigroup
@@ -48,6 +65,5 @@ class HelloSpec extends AnyFunSpec with FunSpecDiscipline with Configuration {
   checkAll(name = "monoid", ruleSet = catsLawsRuleSetMonoid)
 
   checkAll(name = "semilattice", ruleSet = catsLawsRuleSetSemiLattice)
-
 
 }
