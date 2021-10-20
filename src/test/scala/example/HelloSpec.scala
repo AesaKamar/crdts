5734f7bd2ab32f89f28b22cc4b76ef3b300d76bd
AesaKamar <Aesa.Kamar@gmail.com>
2021-10-20 07:59:08 -0400

    separation for clarity

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -11,6 +11,7 @@ import org.typelevel.discipline.scalatest.FunSpecDiscipline
 import org.apache.commons.collections4.trie.PatriciaTrie
 import org.scalacheck.Prop.forAll
 import org.scalatestplus.scalacheck.Checkers
+import simulacrum.typeclass
 
 import java.util
 //import java.util.UUID
@@ -19,14 +20,14 @@ import java.util
 /** Must be:
   *   - A commutative Semigroup with an idempotent combine
   */
-final case class MySemiLattice[A: Monoid](stuff: PatriciaTrie[A])
+final case class MyCRDT[A: Monoid](stuff: PatriciaTrie[A])
 
-object MySemiLattice {
-  implicit def eqInstance[A]: Eq[MySemiLattice[A]] = (x: MySemiLattice[A], y: MySemiLattice[A]) =>
+object MyCRDT {
+  implicit def eqInstance[A]: Eq[MyCRDT[A]] = (x: MyCRDT[A], y: MyCRDT[A]) =>
     x.equals(y)
 
-  implicit def semigroupInstance[A: CommutativeMonoid]: Semigroup[MySemiLattice[A]] =
-    (x: MySemiLattice[A], y: MySemiLattice[A]) => {
+  implicit def semigroupInstance[A: CommutativeMonoid]: Semigroup[MyCRDT[A]] =
+    (x: MyCRDT[A], y: MyCRDT[A]) => {
       val newStuff = new PatriciaTrie(x.stuff)
       y.stuff.forEach { (k, v) =>
         newStuff.merge(
@@ -40,22 +41,23 @@ object MySemiLattice {
         )
         ()
       }
-      MySemiLattice(newStuff)
+      MyCRDT(newStuff)
     }
 
-  implicit def semilatticeInstance[A: CommutativeMonoid]: Semilattice[MySemiLattice[A]] =
-    (x: MySemiLattice[A], y: MySemiLattice[A]) => semigroupInstance[A].combine(x, y)
+  implicit def semilatticeInstance[A: CommutativeMonoid]
+      : Semilattice[MyCRDT[A]] =
+    (x: MyCRDT[A], y: MyCRDT[A]) => semigroupInstance[A].combine(x, y)
 
-  implicit def emptyInstance[A: CommutativeMonoid]: Empty[MySemiLattice[A]] =
-    new Empty[MySemiLattice[A]] {
-      override def empty: MySemiLattice[A] =
-        MySemiLattice(new PatriciaTrie[A]())
+  implicit def emptyInstance[A: CommutativeMonoid]: Empty[MyCRDT[A]] =
+    new Empty[MyCRDT[A]] {
+      override def empty: MyCRDT[A] =
+        MyCRDT(new PatriciaTrie[A]())
     }
 
-  implicit def monoidInstance[A: CommutativeMonoid]: Monoid[MySemiLattice[A]] =
-    new CommutativeMonoid[MySemiLattice[A]] {
-      override def empty: MySemiLattice[A]                                             = emptyInstance[A].empty
-      override def combine(x: MySemiLattice[A], y: MySemiLattice[A]): MySemiLattice[A] =
+  implicit def monoidInstance[A: CommutativeMonoid]: Monoid[MyCRDT[A]] =
+    new CommutativeMonoid[MyCRDT[A]] {
+      override def empty: MyCRDT[A]                               = emptyInstance[A].empty
+      override def combine(x: MyCRDT[A], y: MyCRDT[A]): MyCRDT[A] =
         semigroupInstance[A].combine(x, y)
     }
 }
@@ -64,71 +66,83 @@ final case class RGBA(r: Int, g: Int, b: Int, a: Int)
 object RGBA {
   implicit val eqInstance: Eq[RGBA] = (x: RGBA, y: RGBA) => x == y
 
-  implicit val commutativeMonoidInstance: CommutativeMonoid[RGBA] = new CommutativeMonoid[RGBA] {
-    def empty: RGBA = RGBA(0, 0, 0, 0)
+  implicit val commutativeMonoidInstance: CommutativeMonoid[RGBA] =
+    new CommutativeMonoid[RGBA] {
+      def empty: RGBA = RGBA(0, 0, 0, 0)
 
-    // Use basic color addition
-    val cb: (Int, Int) => Int = (x, y) => math.min(x + y, 255)
+      // Use basic color addition
+      val cb: (Int, Int) => Int = (x, y) => math.min(x + y, 255)
 
-    def combine(x: RGBA, y: RGBA): RGBA =
-      RGBA(cb(x.r, y.r), cb(x.g, y.g), cb(x.b, y.b), cb(x.a, y.a))
+      def combine(x: RGBA, y: RGBA): RGBA =
+        RGBA(cb(x.r, y.r), cb(x.g, y.g), cb(x.b, y.b), cb(x.a, y.a))
 
-  }
+    }
 }
 
 /** Test class
   *
-  * THe plan is to use a patricia tree to gossip diffs about a 2d canvas that people can edit
+  * THe plan is to use a patricia tree to gossip diffs about a 2d canvas that
+  * people can edit
   */
-class HelloSpec extends AnyFunSpec with FunSpecDiscipline with Configuration with Checkers {
+class HelloSpec
+    extends AnyFunSpec
+    with FunSpecDiscipline
+    with Configuration
+    with Checkers {
   import scala.jdk.CollectionConverters._
+  import HelloSpec._
 
-  def mySemiLaticeGenInstance[A: CommutativeMonoid](a: Gen[A]): Gen[MySemiLattice[A]] = for {
-    m <- Gen.mapOf[String, A](Gen.zip(Gen.uuid.map(_.toString), a))
-  } yield {
-    val tree = new PatriciaTrie[A](m.asJava)
-    MySemiLattice(tree)
-  }
-
-  implicit val genLong   = Gen.long
-  implicit val genString = Gen.alphaStr
-
-  val genPatricia: Gen[PatriciaTrie[String]] = for {
-    m <- Gen.mapOf[String, String](Gen.zip(Gen.numStr, Gen.alphaStr))
-  } yield new PatriciaTrie[String](m.asJava)
-
-  def mySemiLaticeOf[A: CommutativeMonoid](a: Gen[A]): Arbitrary[MySemiLattice[A]] =
-    Arbitrary(mySemiLaticeGenInstance[A](a))
-
-  implicit val mySemiLatticeOfString: Arbitrary[MySemiLattice[Int]] =
-    mySemiLaticeOf[Int](Gen.long.map(_.toInt))
-
-  implicit val genRGBA: Gen[RGBA] =
-    Gen.zip(Gen.choose(0, 255), Gen.choose(0, 255), Gen.choose(0, 255), Gen.choose(0, 255)).map {
-      case (r, g, b, a) => RGBA.apply(r, g, b, a)
-    }
-
-  implicit val mySemiLatticeRGBA: Arbitrary[MySemiLattice[RGBA]] =
-    mySemiLaticeOf[RGBA](genRGBA)
-
-  implicit val arbitratyRGBA                  = Arbitrary(genRGBA)
-  val catsLawsSemiLatticeForMySemiLatticeRGBA = SemilatticeTests[MySemiLattice[RGBA]].semilattice
-
-  val catsLawsCommutativeMonoidForRGBA = CommutativeMonoidTests[RGBA].commutativeMonoid
-
-  checkAll(name = "commutativeMonoidRGBA", ruleSet = catsLawsCommutativeMonoidForRGBA)
   checkAll(
-    name = "commutativeMonoidSemiLatticeRGBA",
+    name = "commutativeMonoidRGBA",
+    ruleSet = catsLawsCommutativeMonoidForRGBA
+  )
+  checkAll(
+    name = "semiLatticeRGBA",
     ruleSet = catsLawsSemiLatticeForMySemiLatticeRGBA
   )
+
+  it(
+    "A HashMap should pass the commutativity law under map.merge and CommutativeMonoid[A].combine"
+  ) {
+
+    check(
+      showMapCommutativity(
+        newMap = new util.HashMap[String, String](_),
+        semigroupOp =
+          (l, r) => Order[String].min(l, r).concat(Order[String].max(l, r))
+      )
+    )
+  }
+
+  it(
+    "A PatriciaTrie should pass the commutativity law under map.merge and CommutativeMonoid[A].combine"
+  ) {
+
+    check(
+      showMapCommutativity(
+        newMap = new PatriciaTrie[String](_),
+        semigroupOp =
+          (l, r) => Order[String].min(l, r).concat(Order[String].max(l, r))
+      )
+    )
+  }
+
+}
+
+object HelloSpec {
+  import scala.jdk.CollectionConverters._
 
   def showMapCommutativity[M <: util.AbstractMap[String, String]](
       newMap: util.Map[String, String] => M,
       semigroupOp: (String, String) => String
   ) = {
     forAll(
-      Gen.mapOf[String, String](Gen.zip(Gen.alphaStr, Gen.alphaStr)).map(_.asJava),
-      Gen.mapOf[String, String](Gen.zip(Gen.alphaStr, Gen.alphaStr)).map(_.asJava)
+      Gen
+        .mapOf[String, String](Gen.zip(Gen.alphaStr, Gen.alphaStr))
+        .map(_.asJava),
+      Gen
+        .mapOf[String, String](Gen.zip(Gen.alphaStr, Gen.alphaStr))
+        .map(_.asJava)
     ) { case (x, y) =>
       val xWithY = newMap(x)
       y.forEach { case (a, b) =>
@@ -150,28 +164,48 @@ class HelloSpec extends AnyFunSpec with FunSpecDiscipline with Configuration wit
     }
   }
 
-  it(
-    "A HashMap should pass the commutativity law under map.merge and CommutativeMonoid[A].combine"
-  ) {
+  def mySemiLaticeGenInstance[A: CommutativeMonoid](a: Gen[A]): Gen[MyCRDT[A]] =
+    for {
+      m <- Gen.mapOf[String, A](Gen.zip(Gen.uuid.map(_.toString), a))
+    } yield {
+      val tree = new PatriciaTrie[A](m.asJava)
+      MyCRDT(tree)
+    }
 
-    check(
-      showMapCommutativity(
-        newMap = new util.HashMap[String, String](_),
-        semigroupOp = (l, r) => Order[String].min(l, r).concat(Order[String].max(l, r))
+  implicit val genLong   = Gen.long
+  implicit val genString = Gen.alphaStr
+
+  val genPatricia: Gen[PatriciaTrie[String]] = for {
+    m <- Gen.mapOf[String, String](Gen.zip(Gen.numStr, Gen.alphaStr))
+  } yield new PatriciaTrie[String](m.asJava)
+
+  def mySemiLaticeOf[A: CommutativeMonoid](a: Gen[A]): Arbitrary[MyCRDT[A]] =
+    Arbitrary(mySemiLaticeGenInstance[A](a))
+
+  implicit val mySemiLatticeOfString: Arbitrary[MyCRDT[Int]] =
+    mySemiLaticeOf[Int](Gen.long.map(_.toInt))
+
+  implicit val genRGBA: Gen[RGBA] =
+    Gen
+      .zip(
+        Gen.choose(0, 255),
+        Gen.choose(0, 255),
+        Gen.choose(0, 255),
+        Gen.choose(0, 255)
       )
-    )
-  }
+      .map { case (r, g, b, a) =>
+        RGBA.apply(r, g, b, a)
+      }
 
-  it(
-    "A PatriciaTrie should pass the commutativity law under map.merge and CommutativeMonoid[A].combine"
-  ) {
+  implicit val mySemiLatticeRGBA: Arbitrary[MyCRDT[RGBA]] =
+    mySemiLaticeOf[RGBA](genRGBA)
 
-    check(
-      showMapCommutativity(
-        newMap = new PatriciaTrie[String](_),
-        semigroupOp = (l, r) => Order[String].min(l, r).concat(Order[String].max(l, r))
-      )
-    )
-  }
+  implicit val arbitratyRGBA: Arbitrary[RGBA]  = Arbitrary(genRGBA)
+  val catsLawsSemiLatticeForMySemiLatticeRGBA
+      : SemilatticeTests[MyCRDT[RGBA]]#RuleSet =
+    SemilatticeTests[MyCRDT[RGBA]].semilattice
+
+  val catsLawsCommutativeMonoidForRGBA: CommutativeMonoidTests[RGBA]#RuleSet =
+    CommutativeMonoidTests[RGBA].commutativeMonoid
 
 }
